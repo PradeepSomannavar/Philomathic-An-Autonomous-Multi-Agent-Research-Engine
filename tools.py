080e385ac6922871adec565580a3b2e91b683828
Pradeep Somannavar <pradeep.s0636@gmail.com>
2026-04-20 00:40:36 +0530

    feat: implement web search and scraping tools for agents

--- tools.py
+++ tools.py
@@ -9,7 +9,6 @@ load_dotenv()
 
 tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
-
 @tool
 def web_search(query : str) -> str:
     """Search the web for recent and reliable information on a topic . Returns Titles , URLs and snippets."""
